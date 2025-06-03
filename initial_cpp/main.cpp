16e97d211798a70c0222c4b254fae4aef891e464
dilush hashen <dilushhashen11gmail.com>
2025-06-03 09:27:23 +0530

    added initial_cpp folder

---
+++ initial_cpp/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	// ios::sync_with_stdio(0);
+	// cin.tie(0);
+	
+	int rounds;
+	cin >> rounds;
+
+	for (int r = 0; r < rounds; r++) {
+	}
+}
