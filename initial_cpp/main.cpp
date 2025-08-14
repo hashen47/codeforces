b366cec573ee9aea324cfa9616e9ba30da655cfe
dilush hashen <dilushhashen11@gmail.com>
2025-08-14 18:40:52 +0530

    edit the initial_cpp structure

--- initial_cpp/main.cpp
+++ initial_cpp/main.cpp
@@ -6,9 +6,11 @@ int main() {
 	// ios::sync_with_stdio(0);
 	// cin.tie(0);
 	
-	int rounds;
-	cin >> rounds;
+    // freopen("tests.txt", "r", stdin);
 
-	for (int r = 0; r < rounds; r++) {
+	int testcase_count;
+	cin >> testcase_count;
+
+	for (int t = 0; t < testcase_count; t++) {
 	}
 }
