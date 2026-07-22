90e7391a54b030b42af592725a46e859a6d403ab
dilush hashen <dilushhashen11@gmail.com>
2026-07-22 09:46:02 +0530

    the_67th_integer_problem problem is done

---
+++ the_67th_integer_problem/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+typedef long long i64;
+
+int main(void)
+{
+    int tc_count = 0;
+    std::cin >> tc_count;
+
+    for (int t = 0; t < tc_count; t++)
+    {
+        i64 n = 0;
+        std::cin >> n;
+        std::cout << n << "\n";
+    }
+}
